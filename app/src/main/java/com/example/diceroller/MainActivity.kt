d859bbb42b08304e1a1470a02025d3addd9e2173
Onome <preciousanne2002@gmail.com>
2023-08-07 06:10:31 +0100

    First Push. Dice Images and Button Added

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton:Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+//            Toast.makeText(this, "ButtonClicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val resultNumber = Random().nextInt(6) + 1
+
+        resultText.text = resultNumber.toString()
+    }
+}
